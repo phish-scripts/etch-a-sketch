d94af4c602bd463afa9aed7b0f7570022e8f52fd
Samir Katakamsetty <katakamsettysamir@gmail.com>
2025-07-27 13:37:16 -0400

    Successfully dynamically created a grid using js, divs now change color on hover

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-// creates divs
-function createDiv(amt)
+
+// Creaating a Grid
+let container1 = document.querySelector(".container");
+function createGrid()
 {
-    for (let i = 1; i <= amt; i++)
+    for (let i = 1; i <= 16; i++)
     {
-        let div = document.createElement("div");
-        document.querySelector(".container").append(div);
+        let row = document.createElement("div")
+        row.className = "row";
+        container1.append(row);
+        
+        for (let j = 1; j <= 16; j++)
+        {
+            let column = document.createElement("div");
+            column.className = "column";
+            row.append(column);
+        }
     }
+    console.log("16x16 Grid has been made");
 }
 
-createDiv(16);
+createGrid()
+
+// On Hover, change the color of a div
+
+document.querySelectorAll(".column").forEach(column => column.classList.add("ink"));
