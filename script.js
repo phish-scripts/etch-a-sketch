e2880801b00f5632820babf899e6ec354bc42477
Samir Katakamsetty <katakamsettysamir@gmail.com>
2025-07-28 18:10:05 -0400

    Added functionality to the reset button. The reset button can currently add a new grid to draw on. Fixed the ink logic by making it into a function and moving the logic into drawGrid function

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 
 // Creaating a Grid
 let container1 = document.querySelector(".container");
-function createGrid()
+function createGrid(xcoord, ycoord)
 {
-    for (let i = 1; i <= 16; i++)
+    console.log("running createGrid()")
+    for (let i = 1; i <= xcoord; i++)
     {
         let row = document.createElement("div")
         row.className = "row";
         container1.append(row);
         
-        for (let j = 1; j <= 16; j++)
+        for (let j = 1; j <= ycoord; j++)
         {
             let column = document.createElement("div");
             column.className = "column";
             row.append(column);
         }
     }
-    console.log("16x16 Grid has been made");
+    addInk();
+    console.log("a " + xcoord + "x" + ycoord+" grid has been created.");
 }
 
-createGrid()
+createGrid(16,16)
 
-// On Hover, change the color of a div
+// on hover, change the color of a div
+// document.querySelectorAll(".column").forEach(column => column.classList.add("ink"));
 
-document.querySelectorAll(".column").forEach(column => column.classList.add("ink"));
+const grid = document.querySelectorAll(".column");
+
+// Function that allows a div to be filled with Ink
+function addInk()
+{
+    const grid = document.querySelectorAll(".column");
+    for (box of grid)
+    {
+        box.addEventListener("mouseover", (event) => 
+        { 
+            event.target.classList.add("ink");
+        })
+    }
+}
+
+
+// reset button
+let resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", () => {
+    console.log("Pressed the reset button");
+    let xcoord = window.prompt("How many squares as the width?");
+    let ycoord = window.prompt("How many squares for the height?");
+    console.log("width: " + xcoord);
+    console.log("height: " + ycoord);
+
+    createGrid(xcoord, ycoord);
+
+
+})
+
